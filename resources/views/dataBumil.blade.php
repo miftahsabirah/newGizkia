8dee26f92dd0f22346c6db83b095ddc5086633df
miftahsabirah <miftahsabirah@mail.ugm.ac.id>
2023-11-02 16:36:43 +0700

    baru

--- resources/views/dataBumil.blade.php
+++ resources/views/dataBumil.blade.php
@@ -16,7 +16,7 @@
 
     <div class="flex items-center p-4">
         <button class="text-sm font-normal text-gray-900 dark:text-black mr-2 bg-white-500 text-white px-4 py-2 rounded-lg border border-blue-500 hover:bg-blue-600">
-            Tombol 1
+            Variabel
         </button>
         <button class="text-sm font-normal text-gray-900 dark:text-black mr-2 bg-white-500 text-white px-4 py-2 rounded-lg border border-green-500 hover:bg-green-600">
             Tombol 2
